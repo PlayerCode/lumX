df2621f2624d7a2a5056a4a7da31116e104781fd
Scott Ross <sross@3ce.com>
2017-07-04 11:47:03 -0400

    feat: Add accept scope to file-input directive lx-accept to specify accepted file types.

--- dist/lumx.js
+++ dist/lumx.js
@@ -2596,6 +2596,7 @@
             scope:
             {
                 label: '@lxLabel',
+                accept: '@lxAccept',
                 callback: '&?lxCallback'
             },
             link: link,
@@ -6027,7 +6028,7 @@ angular.module("lumx.dropdown").run(['$templateCache', function(a) { a.put('drop
 angular.module("lumx.file-input").run(['$templateCache', function(a) { a.put('file-input.html', '<div class="input-file">\n' +
     '    <span class="input-file__label">{{ lxFileInput.label }}</span>\n' +
     '    <span class="input-file__filename">{{ lxFileInput.fileName }}</span>\n' +
-    '    <input type="file" class="input-file__input">\n' +
+    '    <input type="file" class="input-file__input" accept="{{lxFileInput.accept}}">\n' +
     '</div>\n' +
     '');
 	 }]);
