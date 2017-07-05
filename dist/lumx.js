f265042813c677fb44fd22af3cda34e28b082284
Scott Ross <sross@3ce.com>
2017-07-05 10:20:23 -0400

    feat file-input: add accept attribute to limit accepted file types

--- dist/lumx.js
+++ dist/lumx.js
@@ -6028,7 +6028,7 @@ angular.module("lumx.dropdown").run(['$templateCache', function(a) { a.put('drop
 angular.module("lumx.file-input").run(['$templateCache', function(a) { a.put('file-input.html', '<div class="input-file">\n' +
     '    <span class="input-file__label">{{ lxFileInput.label }}</span>\n' +
     '    <span class="input-file__filename">{{ lxFileInput.fileName }}</span>\n' +
-    '    <input type="file" class="input-file__input" accept="{{lxFileInput.accept}}">\n' +
+    '    <input type="file" class="input-file__input" accept="{{ lxFileInput.accept }}">\n' +
     '</div>\n' +
     '');
 	 }]);
